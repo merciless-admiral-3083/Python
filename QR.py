8d19f3c45679c82c026b6751bc15224a0e00e432
Jaspreet Singh Nahal <145008487+merciless-admiral-3083@users.noreply.github.com>
2023-10-11 21:02:51 +0530

    Update QR.py

--- QR.py
+++ QR.py
@@ -1,7 +1,7 @@
 import pyqrcode
 import png
 from pyqrcode import QRCode
-a= "https://www.instagram.com/ritu.nahal" #just add the websiite link you want
+a= "https://www.instagram.com/merciless_admiral" #just add the websiite link you want
 url = pyqrcode.create(a)
 url.svg("myqr.svg", scale = 8)
 url.png('myqr.png', scale = 6)
